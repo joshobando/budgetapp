07d2a0ebd5da662e30886d18f20ccbe2b8245eb6
accubility <joshua.obando98@gmail.com>
2021-01-19 18:53:20 -0500

    adding pseudo code

---
+++ server.js
@@ -0,0 +1,3 @@
+//get my requirements
+//need to connect to mongodb for heroku deployment
+//have to use this: "mongodb+srv://joshobando:Obando343@cluster0.bcmns.mongodb.net/budget?retryWrites=true&w=majority"
