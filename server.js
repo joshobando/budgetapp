564163e9130bf77734bd3a6f82160bc01587d2bb
accubility <joshua.obando98@gmail.com>
2021-01-19 19:20:26 -0500

    finished server

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 //get my requirements
+const express = require("express");
+const mongoose = require("mongoose");
+const compression = require("compression")
+const logger = require(morgan);
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(logger("dev"));
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify:false
+});
+
+app.use(require("./routes/api"));
+
+app.listen(PORT, () => {
+    console.log(`App runnign on port ${PORT}`)
+})
 //need to connect to mongodb for heroku deployment
-//have to use this: "mongodb+srv://joshobando:Obando343@cluster0.bcmns.mongodb.net/budget?retryWrites=true&w=majority"
